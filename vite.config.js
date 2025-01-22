000776f3b825edce89e44c867e605e25a7ca816d
JFoster108 <omario2005x@gmail.com>
2025-01-21 19:21:32 -0500

    trying something

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/', // Set base path to match Netlify's root directory
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './src/main.jsx',
-    },
-  },
+  // build: {
+  //   outDir: 'dist',
+  //   rollupOptions: {
+  //     input: './src/main.jsx',
+//     },
+//   },
 });

8a78747781633e27c9386af73e4f4d69d127506b
Justice Foster <omario2005x@gmail.com>
2025-04-30 10:36:33 -0400

    attempted to fix resume button

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,7 +5,7 @@ function Resume() {
     <section id="resume" className="text-center py-8">
   <h2 className="text-3xl font-bold text-pastelBlue mb-4">Resume</h2>
   <a
-    href="./public/resume.pdf"
+    href="/public/resume.pdf"
     download
     className="bg-pastelPurple text-white py-2 px-4 rounded-xl hover:bg-pastelPink shadow-soft"
   >
